ceb20cac8a6b5c8050cdc680d18b83cba668de8f
maurcord <67590886+maurcord@users.noreply.github.com>
2021-03-23 00:50:40 -0400

    added components folder, added style folder, added Assets folder containing images. Added image to Nav bar and App.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import '../Stylesheets/Homepage.css'
+import "../Stylesheets/Homepage.css"
 
 
 class Homepage extends React.Component {
     render() {
         return (
-            <div>
+            <div className="homepage">
             <h1>Welcome! Let me know what you think about my cheese app.</h1>
             </div>
         )
